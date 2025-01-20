25f8d479b466dec6d760edaf8df6080c2eb88d8e
deppep <peppedilillo@gmail.com>
2025-01-20 15:44:55 +0100

    removed pl10k from repo

--- startup.zsh
+++ startup.zsh
@@ -1,6 +1,6 @@
 # colors ls output
 alias ls='ls --color=auto'
-source ~/.config/ghostty/powerlevel10k/powerlevel10k.zsh-theme
+source ~/.powerlevel10k/powerlevel10k.zsh-theme
 
 # To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
-[[ ! -f ~/.config/ghostty/powerlevel10k/p10k.zsh ]] || source ~/.config/ghostty/powerlevel10k/p10k.zsh
+[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
